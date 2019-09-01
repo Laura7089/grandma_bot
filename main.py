22d7b80ed5d60ed63ae41f983aee6edd79fc444b
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2019-09-01 20:07:23 +0100

    Add minimal cli args

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
+import argparse
 import logging
 import os
 
 from grandma_bot import client
 
+# Setup arguments
+parser = argparse.ArgumentParser(description="Run grandma_bot.")
+parser.add_argument(
+    "-l",
+    "--log-file",
+    action="store",
+    default="grandma_bot.log",
+    help="relative path to the logfile",
+)
+parser.add_argument(
+    "-c",
+    "--client-id",
+    action="store",
+    default=os.environ.get("CLIENT_ID"),
+    help=
+    "bot's discord account clientID - not recommended to pass via the CLI!",
+)
+args = parser.parse_args()
+
 # Set up logging
-LOGFILE = "grandma_bot.log"
 LOGGER = logging.getLogger('discord')
 LOGGER.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename=LOGFILE, encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename=args["log-file"],
+                              encoding='utf-8',
+                              mode='w')
 handler.setFormatter(
     logging.Formatter('[%(asctime)s:%(levelname)s:%(name)s]  %(message)s'))
 LOGGER.addHandler(handler)
@@ -16,7 +37,4 @@ LOGGER.addHandler(logging.StreamHandler())
 
 # Initialise a client
 my_client = client.GrandmaClient()
-# with open("clientID.txt") as id_file:
-# myID = id_file.readline().replace("\n", "")
-CLIENT_ID = os.environ.get("CLIENT_ID")
-my_client.run(CLIENT_ID)
+my_client.run(args["client-id"])
